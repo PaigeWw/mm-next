518c2664e405f454b76b475ec9a298c0ed66ee6c
paigeWw <767427364@qq.com>
2020-04-29 00:47:42 +0800

    性能

--- components/select-line.js
+++ components/select-line.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useMemo } from "react"
 import { Flex, Box, Button, Image } from "rebass"
 import { baseUrl } from "../utils/helper"
 import { SerachInput } from "../components/color-and-paint"
@@ -25,154 +25,330 @@ const ImageBox = (props) => {
 		</Box>
 	)
 }
-export default (props) => {
-	const [showScroll] = useState(false)
-	const [showSearch, setShowSearch] = useState(false)
-	const [startIndex, setStartIndex] = useState(0)
-	const [searchQuery, setSearchQuery] = useState("")
-	const [allStyleList, setAllStyleList] = useState([...props.styles])
-	const [showStyleList, setShowStyleList] = useState([...props.styles])
-	const handleChangeStartIndex = (val) => {
-		if (val < 0) return
+
+class SelectLine extends React.Component {
+	constructor(props) {
+		console.log("SelectLine")
+		super(props)
+		this.state = {
+			startIndex: 0,
+			searchQuery: "",
+			showSearch: false,
+		}
+	}
+	shouldComponentUpdate(nextProps, nextStates) {
 		if (
-			val <
-			props.styles.filter((s) => s.styleNo.indexOf(searchQuery) >= 0).length - 2
+			nextProps.styles !== this.props.styles ||
+			nextProps.selectStyles !== this.props.selectStyles ||
+			nextStates.startIndex !== this.state.startIndex ||
+			nextStates.searchQuery !== this.state.searchQuery
 		) {
-			console.log("handleChangeStartIndex", val)
-			setStartIndex(val)
+			// console.log("nextProps.src !== this.props.src")
+			return true
+		} else {
+			return false
 		}
 	}
 
-	useEffect(() => {
-		const temp = allStyleList
-			.filter((s) => s.styleNo.indexOf(searchQuery) >= 0)
-			.slice(startIndex, startIndex + 4)
-		setShowStyleList(temp)
-	}, [startIndex, searchQuery])
-
-	return (
-		<Flex
-			width="100%"
-			minHeight="100px"
-			bg="#DCDCDC"
-			fontSize="0.23rem"
-			color="#fff"
-			sx={{
-				"&:hover": {
-					".innerChild": {
-						visibility: "visible",
-					},
-				},
-			}}
-		>
-			<Flex bg="#000" width="3.22rem" justifyContent="space-between">
-				<Flex
-					flexGrow={1}
-					alignItems="center"
-					justifyContent="center"
-					flexDirection="column"
-					sx={{
-						position: "relative",
-					}}
-				>
-					<Flex alignItems="center" justifyContent="center">
-						{props.kind}{" "}
-						<Box
-							onClick={() => {
-								setShowSearch(!showSearch)
-							}}
-							ml="6px"
-							sx={{
-								//   position: "absolute",
-								cursor: "pointer",
-								width: 0,
-								height: 0,
-								borderLeft: "6px solid transparent",
-								borderRight: "6px solid transparent",
-								borderTop: "6px solid #fff",
-							}}
-						></Box>
-					</Flex>
-
-					{showSearch ? (
-						<Box height="20px" bg="#fff">
-							<SerachInput
-								width="120px"
-								onChange={(e) => {
-									if (!e.target.value) {
-									}
-									setSearchQuery(e.target.value)
-								}}
-							/>{" "}
-						</Box>
-					) : null}
-				</Flex>
-				<Flex
-					alignItems="center"
-					justifyContent="center"
-					width="0.75rem"
-					bg="#313131"
-					className="innerChild"
-					sx={{
-						visibility: "hidden",
-						cursor: "pointer",
-						// visibility: showScroll ? "visible" : "hidden"
-					}}
-					onClick={() => {
-						handleChangeStartIndex(startIndex - 1)
-					}}
-				>
-					<Image src="/3/left.png" width="0.12rem" height="0.21rem"></Image>
-				</Flex>
-			</Flex>
-
-			<Flex flex={1} alignItems="center">
-				{showStyleList.map((item, index) => (
-					<ImageBox
-						item={item}
-						kind={props.kind}
-						col={index}
-						row={props.row}
-						onSelect={props.onSelect}
-						selected={
-							Array.isArray(props.selectStyles) &&
-							props.selectStyles.indexOf(item._id) > -1
-								? true
-								: false
-						}
-						src={item.imgUrl}
-						id={item._id}
-					/>
-				))}
-			</Flex>
+	changeStartIndex(val) {
+		if (val < 0) return
+		const { searchQuery } = this.state
+		const { styles } = this.props
+		if (
+			val <
+			styles.filter((s) => s.styleNo.indexOf(searchQuery) >= 0).length - 2
+		) {
+			console.log("handleChangeStartIndex", val)
+			this.setState({ startIndex: val })
+		}
+	}
+	render() {
+		const { startIndex, searchQuery, showSearch } = this.state
+		const { styles } = this.props
+		return (
 			<Flex
-				bg="#000"
-				width="3.22rem"
-				justifyContent="space-between"
+				width="100%"
+				minHeight="100px"
+				bg="#DCDCDC"
+				fontSize="0.23rem"
+				color="#fff"
 				sx={{
-					background: "no-repeat",
-					backgroundSize: "20px 34px",
-					backgroundPositionY: "center",
-					backgroundPositionX: "70%",
-					// backgroundImage: "url(/3/drop.jpeg)"
+					"&:hover": {
+						".innerChild": {
+							visibility: "visible",
+						},
+					},
 				}}
 			>
+				<Flex bg="#000" width="3.22rem" justifyContent="space-between">
+					<Flex
+						flexGrow={1}
+						alignItems="center"
+						justifyContent="center"
+						flexDirection="column"
+						sx={{
+							position: "relative",
+						}}
+					>
+						<Flex alignItems="center" justifyContent="center">
+							{this.props.kind}{" "}
+							<Box
+								onClick={() => {
+									this.setState({
+										showSearch: !showSearch,
+									})
+									setShowSearch(!showSearch)
+								}}
+								ml="6px"
+								sx={{
+									//   position: "absolute",
+									cursor: "pointer",
+									width: 0,
+									height: 0,
+									borderLeft: "6px solid transparent",
+									borderRight: "6px solid transparent",
+									borderTop: "6px solid #fff",
+								}}
+							></Box>
+						</Flex>
+
+						{showSearch ? (
+							<Box height="20px" bg="#fff">
+								<SerachInput
+									width="120px"
+									onChange={(e) => {
+										if (!e.target.value) {
+										}
+										this.setState({ searchQuery: e.target.value })
+									}}
+								/>{" "}
+							</Box>
+						) : null}
+					</Flex>
+					<Flex
+						alignItems="center"
+						justifyContent="center"
+						width="0.75rem"
+						bg="#313131"
+						className="innerChild"
+						sx={{
+							visibility: "hidden",
+							cursor: "pointer",
+							// visibility: showScroll ? "visible" : "hidden"
+						}}
+						onClick={() => {
+							this.changeStartIndex(startIndex - 1)
+						}}
+					>
+						<Image src="/3/left.png" width="0.12rem" height="0.21rem"></Image>
+					</Flex>
+				</Flex>
+				<Flex flex={1} alignItems="center">
+					{styles
+						.filter((s) => s.styleNo.indexOf(searchQuery) >= 0)
+						.slice(startIndex, startIndex + 4)
+						.map((item, index) => (
+							<ImageBox
+								item={item}
+								kind={this.props.kind}
+								col={index}
+								row={this.props.row}
+								onSelect={this.props.onSelect}
+								selected={
+									Array.isArray(this.props.selectStyles) &&
+									this.props.selectStyles.indexOf(item._id) > -1
+										? true
+										: false
+								}
+								src={item.imgUrl}
+								id={item._id}
+							/>
+						))}
+				</Flex>
 				<Flex
-					className="innerChild"
-					alignItems="center"
-					justifyContent="center"
-					width="0.75rem"
-					bg="#313131"
+					bg="#000"
+					width="3.22rem"
+					justifyContent="space-between"
 					sx={{
-						visibility: showScroll ? "visible" : "hidden",
-					}}
-					onClick={() => {
-						handleChangeStartIndex(startIndex + 1)
+						background: "no-repeat",
+						backgroundSize: "20px 34px",
+						backgroundPositionY: "center",
+						backgroundPositionX: "70%",
+						// backgroundImage: "url(/3/drop.jpeg)"
 					}}
 				>
-					<Image src="/3/right.png" width="0.12rem" height="0.21rem"></Image>
+					<Flex
+						className="innerChild"
+						alignItems="center"
+						justifyContent="center"
+						width="0.75rem"
+						bg="#313131"
+						sx={{
+							visibility: false ? "visible" : "hidden",
+						}}
+						onClick={() => {
+							this.changeStartIndex(startIndex + 1)
+						}}
+					>
+						<Image src="/3/right.png" width="0.12rem" height="0.21rem"></Image>
+					</Flex>
 				</Flex>
 			</Flex>
-		</Flex>
-	)
+		)
+	}
 }
+
+export default SelectLine
+// (props) => {
+// 	const [showScroll] = useState(false)
+// 	const [showSearch, setShowSearch] = useState(false)
+// 	const [startIndex, setStartIndex] = useState(0)
+// 	const [searchQuery, setSearchQuery] = useState("")
+// 	const [allStyleList, setAllStyleList] = useState([...props.styles])
+// 	const [showStyleList, setShowStyleList] = useState([])
+// 	const handleOnSelect = () => {
+// 		props.onSelect()
+// 	}
+// 	const handleChangeStartIndex = (val) => {
+// 		if (val < 0) return
+// 		if (
+// 			val <
+// 			props.styles.filter((s) => s.styleNo.indexOf(searchQuery) >= 0).length - 2
+// 		) {
+// 			console.log("handleChangeStartIndex", val)
+// 			setStartIndex(val)
+// 		}
+// 	}
+
+// 	useEffect(() => {
+// 		const temp = allStyleList
+// 			.filter((s) => s.styleNo.indexOf(searchQuery) >= 0)
+// 			.slice(startIndex, startIndex + 4)
+// 		setShowStyleList(temp)
+// 	}, [startIndex, searchQuery])
+
+// 	return (
+// 		<Flex
+// 			width="100%"
+// 			minHeight="100px"
+// 			bg="#DCDCDC"
+// 			fontSize="0.23rem"
+// 			color="#fff"
+// 			sx={{
+// 				"&:hover": {
+// 					".innerChild": {
+// 						visibility: "visible",
+// 					},
+// 				},
+// 			}}
+// 		>
+// 			<Flex bg="#000" width="3.22rem" justifyContent="space-between">
+// 				<Flex
+// 					flexGrow={1}
+// 					alignItems="center"
+// 					justifyContent="center"
+// 					flexDirection="column"
+// 					sx={{
+// 						position: "relative",
+// 					}}
+// 				>
+// 					<Flex alignItems="center" justifyContent="center">
+// 						{props.kind}{" "}
+// 						<Box
+// 							onClick={() => {
+// 								setShowSearch(!showSearch)
+// 							}}
+// 							ml="6px"
+// 							sx={{
+// 								//   position: "absolute",
+// 								cursor: "pointer",
+// 								width: 0,
+// 								height: 0,
+// 								borderLeft: "6px solid transparent",
+// 								borderRight: "6px solid transparent",
+// 								borderTop: "6px solid #fff",
+// 							}}
+// 						></Box>
+// 					</Flex>
+
+// 					{showSearch ? (
+// 						<Box height="20px" bg="#fff">
+// 							<SerachInput
+// 								width="120px"
+// 								onChange={(e) => {
+// 									if (!e.target.value) {
+// 									}
+// 									setSearchQuery(e.target.value)
+// 								}}
+// 							/>{" "}
+// 						</Box>
+// 					) : null}
+// 				</Flex>
+// 				<Flex
+// 					alignItems="center"
+// 					justifyContent="center"
+// 					width="0.75rem"
+// 					bg="#313131"
+// 					className="innerChild"
+// 					sx={{
+// 						visibility: "hidden",
+// 						cursor: "pointer",
+// 						// visibility: showScroll ? "visible" : "hidden"
+// 					}}
+// 					onClick={() => {
+// 						handleChangeStartIndex(startIndex - 1)
+// 					}}
+// 				>
+// 					<Image src="/3/left.png" width="0.12rem" height="0.21rem"></Image>
+// 				</Flex>
+// 			</Flex>
+// 			<Flex flex={1} alignItems="center">
+// 				{showStyleList.map((item, index) => (
+// 					<ImageBox
+// 						item={item}
+// 						kind={props.kind}
+// 						col={index}
+// 						row={props.row}
+// 						onSelect={props.onSelect}
+// 						selected={
+// 							Array.isArray(props.selectStyles) &&
+// 							props.selectStyles.indexOf(item._id) > -1
+// 								? true
+// 								: false
+// 						}
+// 						src={item.imgUrl}
+// 						id={item._id}
+// 					/>
+// 				))}
+// 			</Flex>
+// 			<Flex
+// 				bg="#000"
+// 				width="3.22rem"
+// 				justifyContent="space-between"
+// 				sx={{
+// 					background: "no-repeat",
+// 					backgroundSize: "20px 34px",
+// 					backgroundPositionY: "center",
+// 					backgroundPositionX: "70%",
+// 					// backgroundImage: "url(/3/drop.jpeg)"
+// 				}}
+// 			>
+// 				<Flex
+// 					className="innerChild"
+// 					alignItems="center"
+// 					justifyContent="center"
+// 					width="0.75rem"
+// 					bg="#313131"
+// 					sx={{
+// 						visibility: showScroll ? "visible" : "hidden",
+// 					}}
+// 					onClick={() => {
+// 						handleChangeStartIndex(startIndex + 1)
+// 					}}
+// 				>
+// 					<Image src="/3/right.png" width="0.12rem" height="0.21rem"></Image>
+// 				</Flex>
+// 			</Flex>
+// 		</Flex>
+// 	)
+// }
--- pages/select.js
+++ pages/select.js
@@ -1,170 +1,170 @@
-import React, { useEffect, useState } from "react";
-import { Flex, Text, Box, Button, Image } from "rebass";
-import Router from "next/router";
-import { ToastContainer, toast } from "../components/commons/toast";
-import arrayMove from "array-move";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import React, { useEffect, useState } from "react"
+import { Flex, Text, Box, Button, Image } from "rebass"
+import Router from "next/router"
+import { ToastContainer, toast } from "../components/commons/toast"
+import arrayMove from "array-move"
+import { SortableContainer, SortableElement } from "react-sortable-hoc"
 
-import Head from "../components/nav";
-import SelectBar from "../components/select-bar";
-import SelectLine from "../components/select-line";
-import SelectAssignTool from "../components/select-assign-tool";
+import Head from "../components/nav"
+import SelectBar from "../components/select-bar"
+import SelectLine from "../components/select-line"
+import SelectAssignTool from "../components/select-assign-tool"
 
-import useUserInfo from "../hooks/getUserInfo";
-import request from "../utils/request.js";
-import { getPageQuery } from "../utils/helper";
+import useUserInfo from "../hooks/getUserInfo"
+import request from "../utils/request.js"
+import { getPageQuery } from "../utils/helper"
 
 export default () => {
-  const info = useUserInfo();
-  // console.log(info)
-  const [categoryList, setGategoryList] = useState([{ styles: [] }]);
-  const [currentSeleted, setCurrentSeleted] = useState(false);
-  const [selectStyles, setSelectStyles] = useState([]);
-  const [selectStylesType, setSelectStylesType] = useState([]);
-  const [queryKey, setQueryKey] = useState({});
+	const info = useUserInfo()
+	// console.log(info)
+	const [categoryList, setGategoryList] = useState([{ styles: [] }])
+	const [currentSeleted, setCurrentSeleted] = useState(false)
+	const [selectStyles, setSelectStyles] = useState([])
+	const [selectStylesType, setSelectStylesType] = useState([])
+	const [queryKey, setQueryKey] = useState({})
 
-  useEffect(() => {
-    const getGategoryList = async () => {
-      let query = getPageQuery();
-      let options = { _id: query.id };
+	useEffect(() => {
+		const getGategoryList = async () => {
+			let query = getPageQuery()
+			let options = { _id: query.id }
 
-      if (queryKey.tags) {
-        options.tag = queryKey.tags;
-        options.styleNo = queryKey.styleNo;
-      }
-      if (queryKey.styleNo) {
-        options.styleNo = queryKey.styleNo;
-      }
+			if (queryKey.tags) {
+				options.tag = queryKey.tags
+				options.styleNo = queryKey.styleNo
+			}
+			if (queryKey.styleNo) {
+				options.styleNo = queryKey.styleNo
+			}
 
-      const req = await request("goods/detail", options, "get");
-      if (!req) return;
-      setGategoryList(req.category);
-    };
-    getGategoryList();
-  }, [queryKey]);
-  // const info = useUserInfo()
-  const handleSelectStyle = (sid, type, top, col, styleItem) => {
-    let pos = selectStyles.indexOf(sid);
-    if (pos > -1) {
-      //已选中 则去除 选中
-      if (selectStyles.length > 1) {
-        let savePos = (pos + 1) % 2;
+			const req = await request("goods/detail", options, "get")
+			if (!req) return
+			setGategoryList(req.category)
+		}
+		getGategoryList()
+	}, [queryKey])
+	// const info = useUserInfo()
+	const handleSelectStyle = (sid, type, top, col, styleItem) => {
+		let pos = selectStyles.indexOf(sid)
+		if (pos > -1) {
+			//已选中 则去除 选中
+			if (selectStyles.length > 1) {
+				let savePos = (pos + 1) % 2
 
-        setSelectStyles(selectStyles.slice(savePos, savePos + 1));
-        setSelectStylesType(selectStylesType.slice(savePos, savePos + 1));
-      } else {
-        setSelectStyles([]);
-        setSelectStylesType([]);
-      }
-      setCurrentSeleted(false);
-    } else {
-      //未选中 则 设置为选中
-      // console.log(selectStyles, type)
-      setCurrentSeleted({ sid, type, top, col, styleItem });
-      let topsPos = selectStylesType.indexOf("TOPS");
-      let bottomsPos = selectStylesType.indexOf("BOTTOMS");
-      if (topsPos > -1 && type == "BOTTOMS") {
-        let saveTemps = selectStyles.slice(topsPos, topsPos + 1);
-        saveTemps.push(sid);
-        setSelectStyles(saveTemps);
-        setSelectStylesType(["TOPS", type]);
-        return;
-      }
-      if (bottomsPos > -1 && type == "TOPS") {
-        let saveTemps = selectStyles.slice(bottomsPos, bottomsPos + 1);
-        saveTemps.push(sid);
-        setSelectStyles(saveTemps);
-        setSelectStylesType(["BOTTOMS", type]);
-        return;
-      }
-      setSelectStyles([sid]);
-      setSelectStylesType([type]);
-    }
-  };
-  const handleChangeQuery = (options) => {
-    // console.log(options)
-    setQueryKey({
-      ...queryKey,
-      ...options,
-    });
-  };
+				setSelectStyles(selectStyles.slice(savePos, savePos + 1))
+				setSelectStylesType(selectStylesType.slice(savePos, savePos + 1))
+			} else {
+				setSelectStyles([])
+				setSelectStylesType([])
+			}
+			setCurrentSeleted(false)
+		} else {
+			//未选中 则 设置为选中
+			// console.log(selectStyles, type)
+			setCurrentSeleted({ sid, type, top, col, styleItem })
+			let topsPos = selectStylesType.indexOf("TOPS")
+			let bottomsPos = selectStylesType.indexOf("BOTTOMS")
+			if (topsPos > -1 && type == "BOTTOMS") {
+				let saveTemps = selectStyles.slice(topsPos, topsPos + 1)
+				saveTemps.push(sid)
+				setSelectStyles(saveTemps)
+				setSelectStylesType(["TOPS", type])
+				return
+			}
+			if (bottomsPos > -1 && type == "TOPS") {
+				let saveTemps = selectStyles.slice(bottomsPos, bottomsPos + 1)
+				saveTemps.push(sid)
+				setSelectStyles(saveTemps)
+				setSelectStylesType(["BOTTOMS", type])
+				return
+			}
+			setSelectStyles([sid])
+			setSelectStylesType([type])
+		}
+	}
+	const handleChangeQuery = (options) => {
+		// console.log(options)
+		setQueryKey({
+			...queryKey,
+			...options,
+		})
+	}
 
-  const SortableItem = SortableElement(({ item, index }) => (
-    <SelectLine
-      key={index + item._id}
-      row={index}
-      kind={item.name}
-      styles={item.styles}
-      selectStyles={selectStyles}
-      onSelect={handleSelectStyle}
-    ></SelectLine>
-  ));
+	const SortableItem = SortableElement(({ item, index }) => (
+		<SelectLine
+			key={index + item._id}
+			row={index}
+			kind={item.name}
+			styles={item.styles}
+			selectStyles={selectStyles}
+			onSelect={handleSelectStyle}
+		></SelectLine>
+	))
 
-  const SortableList = SortableContainer(() => {
-    return (
-      <div>
-        {categoryList.map((item, index) => (
-          <SortableItem
-            key={`categoryList-item-${index}`}
-            index={index}
-            item={item}
-          />
-        ))}
-      </div>
-    );
-  });
-  return (
-    <Flex flexDirection="column">
-      <Head></Head>
-      <ToastContainer />
-      <SelectBar onChangeQuery={handleChangeQuery}></SelectBar>
-      <Box mt="40px" sx={{ position: "relative" }}>
-        <SortableList
-          pressDelay={200}
-          onSortEnd={({ oldIndex, newIndex }) => {
-            setGategoryList(arrayMove(categoryList, oldIndex, newIndex));
-          }}
-        ></SortableList>
+	const SortableList = SortableContainer(() => {
+		return (
+			<div>
+				{categoryList.map((item, index) => (
+					<SortableItem
+						key={`categoryList-item-${index}`}
+						index={index}
+						item={item}
+					/>
+				))}
+			</div>
+		)
+	})
+	return (
+		<Flex flexDirection="column">
+			<Head></Head>
+			<ToastContainer />
+			<SelectBar onChangeQuery={handleChangeQuery}></SelectBar>
+			<Box mt="40px" sx={{ position: "relative" }}>
+				<SortableList
+					pressDelay={200}
+					onSortEnd={({ oldIndex, newIndex }) => {
+						setGategoryList(arrayMove(categoryList, oldIndex, newIndex))
+					}}
+				></SortableList>
 
-        {currentSeleted ? (
-          <SelectAssignTool
-            channels={info.channels}
-            role={info.role}
-            currentSeleted={currentSeleted}
-            onClose={() => {
-              setCurrentSeleted(false);
-            }}
-          />
-        ) : null}
-      </Box>
-      <Button
-        variant="primary"
-        height="1.13rem"
-        width="19.2rem"
-        bg="#FF8E6C"
-        color="#000"
-        padding="0"
-        sx={{
-          borderRadius: 0,
-          fontSize: "0.27rem",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          // console.log('selectStylesselectStylesselectStyles')
-          if (selectStyles.length > 0) {
-            Router.push(
-              `/made?type=${selectStylesType[0]}&id=${selectStyles[0]}${
-                selectStyles.length > 1 ? "&id1=" + selectStyles[1] : ""
-              }`
-            );
-          } else {
-            toast.notify("Please select a style.");
-          }
-        }}
-      >
-        NEXT>
-      </Button>
-      {/* <ToastContainer /> */}
-    </Flex>
-  );
-};
+				{currentSeleted ? (
+					<SelectAssignTool
+						channels={info.channels}
+						role={info.role}
+						currentSeleted={currentSeleted}
+						onClose={() => {
+							setCurrentSeleted(false)
+						}}
+					/>
+				) : null}
+			</Box>
+			<Button
+				variant="primary"
+				height="1.13rem"
+				width="19.2rem"
+				bg="#FF8E6C"
+				color="#000"
+				padding="0"
+				sx={{
+					borderRadius: 0,
+					fontSize: "0.27rem",
+					cursor: "pointer",
+				}}
+				onClick={() => {
+					// console.log('selectStylesselectStylesselectStyles')
+					if (selectStyles.length > 0) {
+						Router.push(
+							`/made?type=${selectStylesType[0]}&id=${selectStyles[0]}${
+								selectStyles.length > 1 ? "&id1=" + selectStyles[1] : ""
+							}`
+						)
+					} else {
+						toast.notify("Please select a style.")
+					}
+				}}
+			>
+				NEXT>
+			</Button>
+			{/* <ToastContainer /> */}
+		</Flex>
+	)
+}
