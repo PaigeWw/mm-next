5fe72312ec4be03d696e37afed2164858740c02b
paigeWw <767427364@qq.com>
2020-07-01 21:27:50 +0800

    to local

--- utils/helper.js
+++ utils/helper.js
@@ -3,8 +3,9 @@ import { parse } from "querystring"
 export const px2rem = (px) => `${px / (750 / 10)}rem`
 export const pptPx2rem = (px) => `${px / (1920 / 10)}rem`
 
-export const baseUrl = "http://8.209.64.159:3001/"
+// export const baseUrl = "http://8.209.64.159:3001/"
 // export const baseUrl = "http://192.168.124.25:3000/"
+export const baseUrl = "http://localhost:3006/"
 
 export const getPageQuery = () => parse(window.location.href.split("?")[1])
 export const guid = () => {
