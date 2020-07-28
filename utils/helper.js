7450535b7270a3f7093d5442cb1a16e9d4a76bf0
wyp10386 <yunpei.wang@uisee.com>
2020-07-28 11:21:04 +0800

    url

--- utils/helper.js
+++ utils/helper.js
@@ -1,18 +1,18 @@
-import { parse } from "querystring"
+import { parse } from "querystring";
 
-export const px2rem = (px) => `${px / (750 / 10)}rem`
-export const pptPx2rem = (px) => `${px / (1920 / 10)}rem`
+export const px2rem = (px) => `${px / (750 / 10)}rem`;
+export const pptPx2rem = (px) => `${px / (1920 / 10)}rem`;
 
-// export const baseUrl = "http://8.209.64.159:3001/"
-export const baseUrl = "/"
+export const baseUrl = "http://8.209.64.159:3001/";
+// export const baseUrl = "/"
 // export const baseUrl = "http://localhost:3001/"
-export const downloadUrl = "http://crm.we-idesign.com/"
+export const downloadUrl = "http://crm.we-idesign.com/";
 
-export const getPageQuery = () => parse(window.location.href.split("?")[1])
+export const getPageQuery = () => parse(window.location.href.split("?")[1]);
 export const guid = () => {
-	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-		var r = (Math.random() * 16) | 0,
-			v = c == "x" ? r : (r & 0x3) | 0x8
-		return v.toString(16)
-	})
-}
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
