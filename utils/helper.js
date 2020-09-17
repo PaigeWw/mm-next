4e8f96cfe5ed58b8122c827bba18e5d3f46bae35
wyp10386 <yunpei.wang@uisee.com>
2020-09-17 11:36:06 +0800

    style

--- utils/helper.js
+++ utils/helper.js
@@ -1,38 +1,38 @@
-import { parse } from "querystring"
+import { parse } from "querystring";
 
-export const px2rem = (px) => `${px / (750 / 10)}rem`
-export const pptPx2rem = (px) => `${px / (1920 / 10)}rem`
+export const px2rem = (px) => `${px / (750 / 10)}rem`;
+export const pptPx2rem = (px) => `${px / (1920 / 10)}rem`;
 
-export const baseUrl = "http://8.209.64.159:3001/"
+export const baseUrl = "http://8.209.64.159:3001/";
 // export const baseUrl = "/"
 // export const baseUrl = "http://localhost:3001/"
 // export const baseUrl = "https://we-idesign.com/"
-export const downloadUrl = "https://crm.we-idesign.com/"
-export const imgUrl = "https://ik.imagekit.io/"
-export const getPageQuery = () => parse(window.location.href.split("?")[1])
+export const downloadUrl = "https://crm.we-idesign.com/";
+export const imgUrl = "https://ik.imagekit.io/";
+export const getPageQuery = () => parse(window.location.href.split("?")[1]);
 export const guid = () => {
-	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-		var r = (Math.random() * 16) | 0,
-			v = c == "x" ? r : (r & 0x3) | 0x8
-		return v.toString(16)
-	})
-}
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
 
 export const filterImageUrl = (url) => {
-	if (!url || !url.split) {
-		return ""
-	}
-	let arrs = url.split("/")
-	// 本地数据
-	if (arrs.length >= 3) {
-		if (url.indexOf(".svg") >= 0) {
-			console.log("svg-url", `/${url}`)
+  if (!url || !url.split) {
+    return "";
+  }
+  let arrs = url.split("/");
+  // 本地数据
+  if (arrs.length >= 3) {
+    if (url.indexOf(".svg") >= 0) {
+      console.log("svg-url", `/${url}`);
 
-			return `https://we-idesign.com/${url}`
-		} else {
-			return `${imgUrl}mrmiss//${arrs[2]}`
-		}
-	} else {
-		return `${imgUrl}${url}`
-	}
-}
+      return `http://8.209.64.159:4000/${url}`;
+    } else {
+      return `${imgUrl}mrmiss//${arrs[2]}`;
+    }
+  } else {
+    return `${imgUrl}${url}`;
+  }
+};
